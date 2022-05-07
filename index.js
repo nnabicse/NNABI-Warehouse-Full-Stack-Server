4c777ed9aa688a5b71c2f675ea4e6e0131759ed5
Nurun Nabi <nnabi.cse@gmail.com>
2022-05-07 12:19:09 +0600

    database connencted

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const cors = require('cors');
 require('dotenv').config();
 const port = process.env.PORT || 5000;
@@ -8,6 +9,21 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+
+
+const uri = `mongodb+srv://{process.env.DB_USER}:{DB_PASS}@allitems.durq4.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+    const collection = client.db("test").collection("devices");
+
+    console.log("Database Connected");
+    // perform actions on the collection object
+    client.close();
+});
+
+
+
+
 app.get('/', (req, res) => {
     res.send('Server Running');
 });
@@ -15,3 +31,7 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
     console.log("Listening to Port", port)
 })
+
+
+//user
+//1NHR2v66opcEpcDd
