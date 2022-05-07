6b56c64e04fb5e9661578127cf3181017c20a886
Nurun Nabi <nnabi.cse@gmail.com>
2022-05-07 19:57:02 +0600

    increasing & decreasing stock items implemented

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const cors = require('cors');
 require('dotenv').config();
 const port = process.env.PORT || 5000;
@@ -11,15 +11,52 @@ app.use(express.json());
 
 
 
-const uri = `mongodb+srv://{process.env.DB_USER}:{DB_PASS}@allitems.durq4.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://user:1NHR2v66opcEpcDd@allitems.durq4.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-    const collection = client.db("test").collection("devices");
 
-    console.log("Database Connected");
-    // perform actions on the collection object
-    client.close();
-});
+async function run() {
+    try {
+        await client.connect();
+        console.log("DB Connected");
+        const itemCollection = client.db("allitem").collection("inventory-item");
+
+        app.get('/item', async (req, res) => {
+            const query = {};
+            const cursor = itemCollection.find(query);
+            const items = await cursor.toArray();
+            res.send(items);
+        });
+        app.get('/item/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const item = await itemCollection.findOne(query);
+            res.send(item);
+        });
+        app.put('/item/:id', async (req, res) => {
+            const qty = req.body;
+            console.log(qty);
+            const id = req.params.id;
+
+            const filter = { _id: ObjectId(id) };
+            const update = {
+                $set: {
+                    quantity: qty.newQuantity
+
+                }
+            }
+            const result = await itemCollection.updateOne(filter, update);
+            res.send(result);
+            console.log(result);
+        });
+
+    }
+    finally {
+
+    }
+
+}
+
+run().catch(console.dir);
 
 
 
